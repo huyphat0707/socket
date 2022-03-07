79a519a6725de7cf24e57b5c3783a1b75c3abef7
cris <haphat0707@gmai.coml>
2022-03-07 11:05:55 +0700

    master fix bug

--- index.js
+++ index.js
@@ -1,24 +1,30 @@
-const httpServer = require('http').createServer();
-const socketIO = require('socket.io')(httpServer);
-
-socketIO.on('connection', function (client) {
-    console.log('Connected...', client.id);
-
-    client.on('message', function name(data) {
-        console.log(data);
-        socketIO.emit('message', data)
+const app = require('express')()
+const httpServer = require('http').createServer(app)
+const io = require('socket.io')(httpServer);
+app.get('/', (req, res) => {
+    res.send("Node Server is running. Yay!!")
+})
+io.on('connection', socket => {
+    //Get the chatID of the user and join in a room of the same chatID
+    chatID = socket.handshake.query.chatID
+    socket.join(chatID)
+    //Leave the room if the user closes the socket
+    socket.on('disconnect', () => {
+        socket.leave(chatID)
     })
-
-    client.on('disconnect', function () {
-        console.log('Disconnected...', client.id);
-    })
-
-    client.on('error', function (err) {
-        console.log('Error detected', client.id);
-        console.log(err);
+    //Send message to only a particular user
+    socket.on('send_message', message => {
+        receiverChatID = message.receiverChatID
+        senderChatID = message.senderChatID
+        content = message.content
+        //Send message to only that particular room
+        socket.in(receiverChatID).emit('receive_message', {
+            'content': content,
+            'senderChatID': senderChatID,
+            'receiverChatID':receiverChatID,
+        })
     })
 });
-
 const port = process.env.PORT || 3000;
 httpServer.listen(port, function (err) {
     if (err) console.log(err);
